76cd201e83d5139e2fdcf4bb62950110cf287dbe
lillypad <lillypadgirl86@gmail.com>
2019-11-22 22:46:04 -0400

    c2dec update

--- src/kp2dc/src/main.c
+++ src/kp2dc/src/main.c
@@ -6,27 +6,45 @@
 #include "b64/b64.h"
 
 #define MAX_STR_LEN 1024
+#define VERSION "v1.0"
 
 void help_menu(){
   char menu[] =
 
-    "/-------------------------------------------------------\\\n"
-    "|              KPot v2.0 Decrypt/Encrypt                |\n"
-    "|-------------------------------------------------------|\n"
-    "|  -h, --help             Print this Help Menu          |\n"
-    "|  -a, --all     [sample] Dump All Strings              |\n"
-    "|  -k, --key     [sample] Dump C2 Server Decryption Key |\n"
-    "|  -d, --domain  [sample] Dump C2 Domain                |\n"
-    "|  --set-key     [keystr] Set Decryption Key            |\n"
-    "|  -c, --c2      [base64] Decrypt C2 base64             |\n"
-    "|  -o, --output  [_file_] Output to File                |\n"
-    "|  -i, --input   [_file_] Input File                    |\n"
-    "|  --std-in               Use stdin for -c, --c2        |\n"
-    "|-------------------------------------------------------|\n"
-    "| Company: GoSecure TITAN                               |\n"
-    "| Author : Lilly Chalupowski                            |\n"
-    "\\-------------------------------------------------------/\n";
+    "/----------------------------------------------------------\\\n"
+    "|              KPot v2.0 Decrypt/Encrypt                   |\n"
+    "|----------------------------------------------------------|\n"
+    "|  -h, --help                Print this Help Menu          |\n"
+    "|  --decrypt-strings         Dump All Strings              |\n"
+    "|  -q, --quiet               Silence Output                |\n"
+    "|  --extract-key             Dump C2 Server Decryption Key |\n"
+    "|  --extract-domain          Dump C2 Domain                |\n"
+    "|  --decrypt-sid      [#]    Decrypt String by ID          |\n"
+    "|  -k, --key        [keystr] Crypt Key String              |\n"
+    "|  --crypt-file              Crypt File                    |\n"
+    "|  -o, --output     [_file_] Output to File                |\n"
+    "|  -i, --input      [_file_] Input File                    |\n"
+    "|  --crypt-stdin             StdIn (base64 only)           |\n"
+    "|  --examples                Show Examples                 |\n"
+    "|  -v, --version             Show Version                  |\n"
+    "|----------------------------------------------------------|\n"
+    "| Company: GoSecure TITAN                                  |\n"
+    "| Author : Lilly Chalupowski                               |\n"
+    "\\----------------------------------------------------------/\n";
   printf("%s", menu);
+}
+
+void examples(){
+  char examples[] =
+    "kp2dc --extract-key -i sample.bin\n"
+    "kp2dc --extract-domain -i sample.bin\n"
+    "kp2dc --decrypt-strings -i sample.bin\n"
+    "kp2dc --examples\n"
+    "kp2dc --crypt-file -i data.bin --key [KEY] -o out.bin\n"
+    "cat c2.b64 | kp2dc --crypt-stdin --key [KEY] -o out.bin\n"
+    "kp2dc --decrypt-strings --sid-low 0 --sid-high 182 -i sample.bin\n"
+    "kp2dc --decrypt-sid [SID#] -i sample.bin\n";
+  printf("%s", examples);
 }
 
 int iFileSize(FILE *fp){
@@ -107,141 +125,246 @@ bool bWriteFile(char *fn, void *pData, int iDataSize){
   return true;
 }
 
-// Function Definitions
-int32_t *sd0(int32_t *sb, int32_t sid, int32_t *bin);
-int32_t *c2dec(int32_t *data, int32_t data_size, int32_t *key, int32_t key_length);
+struct args_t{
+  bool bStdIn;
+  char *pOutputFile;
+  char *pKey;
+  bool bAllStrings;
+  bool bDomain;
+  bool bExtractKey;
+  bool bDecryptFile;
+  int32_t iSid;
+  int32_t iSidLow;
+  int32_t iSidHigh;
+  int32_t iInputFileSize;
+  void *pInputFile;
+  bool bQuiet;
+};
 
-int main(int argc, char **argv){
-  bool bStdIn = false;
-  char *pOutputFile= NULL;
-  char *pKey = NULL;
-  bool bAllStrings = false;
-  bool bDomain = false;
-  bool bExtractKey = false;
-  bool bDecryptFile = false;
-  int32_t iSid = -1;
-  int32_t iSidLow = 0;
-  int32_t iSidHigh = 182;
-  int32_t iInputFileSize = 0;
-  void *pInputFile = NULL;
+struct args_t *pArgsInit(){
+  struct args_t *args = malloc(sizeof(struct args_t));
+  args->bStdIn = false;
+  args->pOutputFile = NULL;
+  args->pKey = NULL;
+  args->bAllStrings = NULL;
+  args->bDomain = false;
+  args->bExtractKey = false;
+  args->bDecryptFile = false;
+  args->iSid = -1;
+  args->iSidLow = 0;
+  args->iSidHigh = 182;
+  args->iInputFileSize = 0;
+  args->pInputFile = NULL;
+  args->bQuiet = false;
+  return args;
+}
+
+void ArgsCleanup(struct args_t *pArgs){
+  if (pArgs->pInputFile != NULL){
+    free(pArgs->pInputFile);
+  }
+  free(pArgs);
+}
+
+int iArgsParse(int argc, char **argv, struct args_t *pArgs){
   if (argc <= 1){
     help_menu();
     fprintf(stderr, "Not enough arguments...\n");
     return 1;
   }
   for (int a = 0; a < argc; a++){
+    if (strcmp(argv[a], "--examples") == 0){
+      examples();
+    }
+    if (strcmp(argv[a], "-v") == 0 || strcmp(argv[a], "--version") == 0){
+      printf("%s\n", VERSION);
+    }
+    if (strcmp(argv[a], "--quit") == 0 ||
+        strcmp(argv[a], "-q") == 0){
+      pArgs->bQuiet = true;
+    }
     if (strcmp(argv[a], "--crypt-stdin") == 0){
-      bStdIn = true;
+      pArgs->bStdIn = true;
     }
     if (strcmp(argv[a], "--sid-low") == 0){
-      iSidLow = atoi(argv[a+1]);
+      pArgs->iSidLow = atoi(argv[a+1]);
     }
     if (strcmp(argv[a], "--sid-high") == 0){
-      iSidHigh = atoi(argv[a+1]);
+      pArgs->iSidHigh = atoi(argv[a+1]);
     }
     if (strcmp(argv[a], "--decrypt-sid") == 0){
-      iSid = atoi(argv[a+1]);
+      pArgs->iSid = atoi(argv[a+1]);
     }
-    if (strcmp(argv[a], "-k") == 0 || strcmp(argv[a], "--key") == 0){
-      pKey = argv[a+1];
+    if (strcmp(argv[a], "-k") == 0 ||
+        strcmp(argv[a], "--key") == 0){
+      pArgs->pKey = argv[a+1];
     }
-    if (strcmp(argv[a], "-i") == 0 || strcmp(argv[a], "--input") == 0){
-      pInputFile = pReadFile(argv[a+1]);
-      if (pInputFile == NULL){
+    if (strcmp(argv[a], "-i") == 0 ||
+        strcmp(argv[a], "--input") == 0){
+      pArgs->pInputFile = pReadFile(argv[a+1]);
+      if (pArgs->pInputFile == NULL){
         return 1;
       }
-      iInputFileSize = iGetFileSize(argv[a+1]);
+      pArgs->iInputFileSize = iGetFileSize(argv[a+1]);
     }
-    if (strcmp(argv[a], "-o") == 0 || strcmp(argv[a], "--output") == 0){
-      pOutputFile = argv[a+1];
+    if (strcmp(argv[a], "-o") == 0 ||
+        strcmp(argv[a], "--output") == 0){
+      pArgs->pOutputFile = argv[a+1];
     }
-    if (strcmp(argv[a], "-h") == 0 || strcmp(argv[a], "--help") == 0){
+    if (strcmp(argv[a], "-h") == 0 ||
+        strcmp(argv[a], "--help") == 0){
       help_menu();
       return 0;
     }
     if (strcmp(argv[a], "--decrypt-strings") == 0){
-      bAllStrings = true;
+      pArgs->bAllStrings = true;
     }
     if (strcmp(argv[a], "--extract-domain") == 0){
-      bDomain = true;
+      pArgs->bDomain = true;
     }
     if (strcmp(argv[a], "--extract-key") == 0){
-      bExtractKey = true;
+      pArgs->bExtractKey = true;
     }
     if (strcmp(argv[a], "--crypt-file") == 0){
-      bDecryptFile = true;
+      pArgs->bDecryptFile = true;
     }
   }
-  if (iSidLow > iSidHigh || iSid < -1){
+  if (pArgs->iSidLow > pArgs->iSidHigh || pArgs->iSid < -1){
     fprintf(stderr, "String ID values invalid.\n");
     return 1;
   }
-  if (bAllStrings == true && pInputFile!= NULL){
+  return 0;
+}
+
+int32_t *sd0(int32_t *sb, int32_t sid, int32_t *bin);
+int32_t *c2dec(int32_t *data, int32_t data_size, int32_t *key, int32_t key_length);
+
+int iModuleAllStrings(struct args_t *pArgs){
+  if (pArgs->bAllStrings == true && pArgs->pInputFile!= NULL){
     void *sb = malloc(MAX_STR_LEN);
-    for (int i = iSidLow; i < iSidHigh; i++){
+    for (int i = pArgs->iSidLow; i < pArgs->iSidHigh; i++){
       memset(sb, 0, MAX_STR_LEN);
-      sd0(sb, i, pInputFile);
+      sd0(sb, i, pArgs->pInputFile);
       printf("sid:0x%x", i);
       hex_dump("", sb, strlen(sb) + 1);
     }
     free(sb);
-    free(pInputFile);
     return 0;
   }
-  if (bExtractKey == true && pInputFile != NULL){
+  return 1;
+}
+
+int iModuleExtractKey(struct args_t *pArgs){
+  if (pArgs->bExtractKey == true && pArgs->pInputFile != NULL){
     void *sb = malloc(MAX_STR_LEN);
     memset(sb, 0, MAX_STR_LEN);
-    sd0(sb, 2, pInputFile);
-    printf("%s\n", (char *)sb);
+    sd0(sb, 2, pArgs->pInputFile);
+    if (pArgs->bQuiet != true){
+      printf("%s\n", (char *)sb);
+    }
+    if (pArgs->pOutputFile != NULL){
+      if (bWriteFile(pArgs->pOutputFile, sb, strlen(sb)) == false){
+        return 1;
+      }
+    }
     free(sb);
-    free(pInputFile);
     return 0;
   }
-  if (bDomain == true && pInputFile != NULL){
+  return 1;
+}
+
+int iModuleExtractDomain(struct args_t *pArgs){
+  if (pArgs->bDomain == true && pArgs->pInputFile != NULL){
     void *sb = malloc(MAX_STR_LEN);
     memset(sb, 0, MAX_STR_LEN);
-    sd0(sb, 0, pInputFile);
-    printf("%s\n", (char *)sb);
+    sd0(sb, 0, pArgs->pInputFile);
+    if (pArgs->bQuiet != true){
+      printf("%s\n", (char *)sb);
+    }
+    if (pArgs->pOutputFile != NULL){
+      if (bWriteFile(pArgs->pOutputFile, sb, strlen(sb)) == false){
+        return 1;
+      }
+    }
     free(sb);
-    free(pInputFile);
     return 0;
   }
-  if (bStdIn == true && pKey != NULL){
+  return 1;
+}
+
+int iModuleStdIn(struct args_t *pArgs){
+  if (pArgs->bStdIn == true && pArgs->pKey != NULL){
     char *txt = malloc(MAX_STR_LEN);
     memset(txt, 0, MAX_STR_LEN);
     fgets(txt, MAX_STR_LEN, stdin);
-    printf("C2 Server Base64 String:\n");
-    printf("%s\n", txt);
+    if (pArgs->bQuiet != true){
+      printf("C2 Server Base64 String:\n");
+      printf("%s\n", txt);
+    }
     void *dec = base64_decode(txt);
-    hex_dump("C2 Base64 Decoded String", dec, base64_output_size(txt));
-    c2dec(dec, base64_output_size(txt), (int32_t *)pKey, strlen(pKey));
-    hex_dump("decrypted", dec, base64_output_size(txt));
+    if (pArgs->bQuiet != true){
+      hex_dump("C2 Base64 Decoded String", dec, base64_output_size(txt));
+    }
+    c2dec(dec, base64_output_size(txt), (int32_t *)pArgs->pKey, strlen(pArgs->pKey));
+    if (pArgs->bQuiet != true){
+      hex_dump("decrypted", dec, base64_output_size(txt));
+    }
+    if (pArgs->pOutputFile != NULL){
+      if (bWriteFile(pArgs->pOutputFile, dec, base64_output_size(txt)) == false){
+        return 1;
+      }
+    }
     free(txt);
     free(dec);
     return 0;
   }
-  if (iSid != -1 && iSid >= 0 && pInputFile != NULL){
+  return 1;
+}
+
+int iModuleSID(struct args_t *pArgs){
+  if (pArgs->iSid != -1 && pArgs->iSid >= 0 && pArgs->pInputFile != NULL){
     void *sb = malloc(MAX_STR_LEN);
     memset(sb, 0, MAX_STR_LEN);
-    sd0(sb, iSid, pInputFile);
-    printf("%s\n", (char *)sb);
+    sd0(sb, pArgs->iSid, pArgs->pInputFile);
+    if (pArgs->bQuiet != true){
+      printf("%s\n", (char *)sb);
+    }
     free(sb);
-    free(pInputFile);
     return 0;
   }
-  if (bDecryptFile == true && pInputFile != NULL && pKey != NULL){
-    hex_dump("encrypted", pInputFile, iInputFileSize);
-    c2dec(pInputFile, iInputFileSize, (int32_t *)pKey, strlen(pKey));
-    hex_dump("decrypted", pInputFile, iInputFileSize);
-    if (pOutputFile != NULL){
-      if (bWriteFile(pOutputFile, pInputFile, iInputFileSize) == false){
+  return 1;
+}
+
+int iModuleCryptFile(struct args_t *pArgs){
+  if (pArgs->bDecryptFile == true && pArgs->pInputFile != NULL && pArgs->pKey != NULL){
+    if (pArgs->bQuiet != true){
+      hex_dump("encrypted", pArgs->pInputFile, pArgs->iInputFileSize);
+    }
+    c2dec(pArgs->pInputFile, pArgs->iInputFileSize, (int32_t *)pArgs->pKey, strlen(pArgs->pKey));
+    if (pArgs->bQuiet != true){
+      hex_dump("decrypted", pArgs->pInputFile, pArgs->iInputFileSize);
+    }
+    if (pArgs->pOutputFile != NULL){
+      if (bWriteFile(pArgs->pOutputFile, pArgs->pInputFile, pArgs->iInputFileSize) == false){
         return 1;
       }
     }
-    free(pInputFile);
     return 0;
   }
-  help_menu();
-  fprintf(stderr, "Not enough arguments...\n");
   return 1;
+}
+
+int main(int argc, char **argv){
+  struct args_t *pArgs = pArgsInit();
+  if (iArgsParse(argc, argv, pArgs) == 1){
+    return 1;
+  }
+  iModuleAllStrings(pArgs);
+  iModuleExtractKey(pArgs);
+  iModuleExtractDomain(pArgs);
+  iModuleStdIn(pArgs);
+  iModuleSID(pArgs);
+  iModuleCryptFile(pArgs);
+  ArgsCleanup(pArgs);
+  return 0;
 }
