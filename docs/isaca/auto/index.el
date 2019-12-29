e33d81ab2163a49e023ca0d89b53d06b790d4a8d
lillypad <lillypadgirl86@gmail.com>
2019-12-28 22:59:06 -0400

    art into science

---
+++ docs/isaca/auto/index.el
@@ -0,0 +1,33 @@
+(TeX-add-style-hook
+ "index"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("beamer" "aspectratio=169")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("textpos" "absolute" "overlay")))
+   (add-to-list 'LaTeX-verbatim-environments-local "minted")
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "beamer"
+    "beamer10"
+    "tabularx"
+    "graphicx"
+    "eso-pic"
+    "minted"
+    "hyperref"
+    "tcolorbox"
+    "textpos")
+   (TeX-add-symbols
+    '("AtPagemyUpperLeft" 1))
+   (LaTeX-add-environments
+    "myitemize"))
+ :latex)
+
